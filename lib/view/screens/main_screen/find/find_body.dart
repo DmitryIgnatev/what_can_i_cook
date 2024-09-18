baf1bf38feaace1a4e8ebc72f1352534e0e164e0
DmitryIgnatev <dmitryignatevdev@gmail.com>
2024-09-19 00:27:11 +0400

    feat: add crashlitics for app

--- lib/view/screens/main_screen/find/find_body.dart
+++ lib/view/screens/main_screen/find/find_body.dart
@@ -33,119 +33,115 @@ class _FindBodyState extends State<FindBody> {
       child: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(5),
-          child: Expanded(
-            child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  const NewCategoryList(),
-                  const NewTagList(),
-                  const FindIngreidentsModule(),
-                  BlocBuilder<RecipeBloc, RecipeState>(
-                    builder: (context, state) {
-                      return StreamBuilder<List<Recipe>>(
-                        stream:
-                            ReadStore().readData('recipes', Recipe.fromJson),
-                        builder: (BuildContext context, snapshot) {
-                          if (!snapshot.hasData) {
-                            return const Center(child: Text('Нет записей'));
-                          } else {
-                            final List<Recipe> recipes = snapshot.data!
-                                .where(
-                                  (thisRecipe) =>
-                                      thisRecipe.ingredients.any(
-                                        (element) =>
-                                            state.ingredients.contains(element),
-                                      ) &&
-                                      thisRecipe.category == state.category,
-                                )
-                                .toList();
-                            return ListView.builder(
-                              shrinkWrap: true,
-                              itemCount: recipes.length,
-                              physics: const NeverScrollableScrollPhysics(),
-                              itemBuilder: (BuildContext context, int index) {
-                                return Padding(
-                                  padding: const EdgeInsets.all(5),
-                                  child: GestureDetector(
-                                    onTap: () {
-                                      Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                          builder: (context) => SelectedRecipe(
-                                            pageIndex: index,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                const NewCategoryList(),
+                const NewTagList(),
+                const FindIngreidentsModule(),
+                BlocBuilder<RecipeBloc, RecipeState>(
+                  builder: (context, state) {
+                    return StreamBuilder<List<Recipe>>(
+                      stream: ReadStore().readData('recipes', Recipe.fromJson),
+                      builder: (BuildContext context, snapshot) {
+                        if (!snapshot.hasData) {
+                          return const Center(child: Text('Нет записей'));
+                        } else {
+                          final List<Recipe> recipes = snapshot.data!
+                              .where(
+                                (thisRecipe) =>
+                                    thisRecipe.ingredients.any(
+                                      (element) =>
+                                          state.ingredients.contains(element),
+                                    ) &&
+                                    thisRecipe.category == state.category,
+                              )
+                              .toList();
+                          return ListView.builder(
+                            shrinkWrap: true,
+                            itemCount: recipes.length,
+                            physics: const NeverScrollableScrollPhysics(),
+                            itemBuilder: (BuildContext context, int index) {
+                              return Padding(
+                                padding: const EdgeInsets.all(5),
+                                child: GestureDetector(
+                                  onTap: () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) => SelectedRecipe(
+                                          pageIndex: index,
+                                        ),
+                                      ),
+                                    );
+                                  },
+                                  child: SizedBox(
+                                    height: 90,
+                                    child: Row(
+                                      children: [
+                                        ClipRRect(
+                                          borderRadius: const BorderRadius.all(
+                                            Radius.circular(14),
+                                          ),
+                                          child: SizedBox(
+                                            width: 0.30 * width,
+                                            child: FuturePicture(
+                                              pictureUrl:
+                                                  recipes[index].pictureUrl,
+                                            ),
                                           ),
                                         ),
-                                      );
-                                    },
-                                    child: SizedBox(
-                                      height: 90,
-                                      child: Row(
-                                        children: [
-                                          ClipRRect(
-                                            borderRadius:
-                                                const BorderRadius.all(
-                                              Radius.circular(14),
-                                            ),
-                                            child: SizedBox(
-                                              width: 0.30 * width,
-                                              child: FuturePicture(
-                                                pictureUrl:
-                                                    recipes[index].pictureUrl,
+                                        Padding(
+                                          padding: const EdgeInsets.fromLTRB(
+                                            5,
+                                            2,
+                                            0,
+                                            0,
+                                          ),
+                                          child: Column(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            children: [
+                                              Text(
+                                                recipes[index].name,
+                                                style: const TextStyle(
+                                                  fontWeight: FontWeight.w600,
+                                                  fontSize: 20,
+                                                ),
                                               ),
-                                            ),
+                                              SizedBox(
+                                                height: 20,
+                                                width: 0.5 * width,
+                                                child: Text(
+                                                  recipes[index]
+                                                      .ingredients
+                                                      .toString()
+                                                      .replaceAll('[', '')
+                                                      .replaceAll(']', ''),
+                                                ),
+                                              ),
+                                              const SizedBox(
+                                                height: 10,
+                                              ),
+                                              Text(
+                                                'Время на приготовление: ${recipes[index].time ~/ 60} ч ${recipes[index].time % 60} минут',
+                                              ),
+                                            ],
                                           ),
-                                          Padding(
-                                            padding: const EdgeInsets.fromLTRB(
-                                              5,
-                                              2,
-                                              0,
-                                              0,
-                                            ),
-                                            child: Column(
-                                              crossAxisAlignment:
-                                                  CrossAxisAlignment.start,
-                                              children: [
-                                                Text(
-                                                  recipes[index].name,
-                                                  style: const TextStyle(
-                                                    fontWeight: FontWeight.w600,
-                                                    fontSize: 20,
-                                                  ),
-                                                ),
-                                                SizedBox(
-                                                  height: 20,
-                                                  width: 0.5 * width,
-                                                  child: Text(
-                                                    recipes[index]
-                                                        .ingredients
-                                                        .toString()
-                                                        .replaceAll('[', '')
-                                                        .replaceAll(']', ''),
-                                                  ),
-                                                ),
-                                                const SizedBox(
-                                                  height: 10,
-                                                ),
-                                                Text(
-                                                  'Время на приготовление: ${recipes[index].time ~/ 60} ч ${recipes[index].time % 60} минут',
-                                                ),
-                                              ],
-                                            ),
-                                          ),
-                                        ],
-                                      ),
+                                        ),
+                                      ],
                                     ),
                                   ),
-                                );
-                              },
-                            );
-                          }
-                        },
-                      );
-                    },
-                  ),
-                ],
-              ),
+                                ),
+                              );
+                            },
+                          );
+                        }
+                      },
+                    );
+                  },
+                ),
+              ],
             ),
           ),
         ),
